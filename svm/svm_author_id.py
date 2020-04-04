bb75c08988fa0abbcb8565ac6650ba9fab63f4db
jonasthiel <41738638+jonasthiel@users.noreply.github.com>
2020-04-04 18:52:53 +0200

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+
+features_train = features_train[:round(len(features_train)/100)]
+labels_train = labels_train[:round(len(labels_train)/100)]
+clf = SVC(kernel='rbf', C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+t0 = time()
+pred = clf.predict(features_test)
+print("prediction time:", round(time()-t0, 3), "s")
+print("prediction for element 10, 26, and 50:", pred[10], pred[26], pred[50])
+print("sum of test events predicted for Chris:", sum(pred))
+
+acc = clf.score(features_test, labels_test)
+print("accuracy", acc)
 
 #########################################################
 
