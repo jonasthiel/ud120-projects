87c21b0e2c838333cc91be011e1c1302077bf08e
jonasthiel <41738638+jonasthiel@users.noreply.github.com>
2020-04-04 19:01:52 +0200

    Update vectorize_text.py

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,35 +41,45 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        #temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            print (path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            temp = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            for i in ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]:
+                temp = temp.replace(i, "")
 
             ### append the text to word_data
+            word_data.append(temp)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
+            if name == "Sara":
+                from_data.append(0)
+            if name == "Chris":
+                from_data.append(1)
+                
             email.close()
 
-print "emails processed"
+print ("emails processed")
+print("string for word_data[152]:", word_data[152])
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
-
-
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
 
 ### in Part 4, do TfIdf vectorization here
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(word_data)
+print("word count:", len(vectorizer.get_feature_names()))
+print("word number 34597:", vectorizer.get_feature_names()[34597])
 
