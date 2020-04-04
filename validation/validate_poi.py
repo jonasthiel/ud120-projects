0ed1266d3435772e80661f34d135de208767d64a
jonasthiel <41738638+jonasthiel@users.noreply.github.com>
2020-04-04 19:20:58 +0200

    Update validate_poi.py

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -15,7 +15,7 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -28,5 +28,21 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+acc = clf.score(features, labels)
+
+print("accuracy of decision tree with default parameters:", acc)
+
+from sklearn import model_selection
+
+features_train, features_test, labels_train, labels_test = model_selection.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+acc = clf.score(features_test, labels_test)
+
+print("accuracy of decision tree with testing regime:", acc)
 
