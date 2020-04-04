326fca24ff8e057e66b809728bcc6ce3f59397b1
jonasthiel <41738638+jonasthiel@users.noreply.github.com>
2020-04-04 18:13:48 +0200

    Update poi_id.py

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -10,16 +10,32 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+
+### Initial features_list
+#features_list = ['poi','salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees', 'to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
+
+features_list = ['poi', 'total_payments', 'total_stock_value', 'from_poi_to_this_person', 'to_messages_poi_share', 'from_messages_poi_share']
 
 ### Load the dictionary containing the dataset
-with open("final_project_dataset.pkl", "r") as data_file:
+with open("final_project_dataset.pkl", "rb") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 'TOTAL')
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
+
+for i in my_dataset:
+    if my_dataset[i]['from_this_person_to_poi'] != 'Nan' and my_dataset[i]['from_messages'] != 'NaN':
+        my_dataset[i]['to_messages_poi_share'] = int(my_dataset[i]['from_this_person_to_poi']) / (int(my_dataset[i]['from_messages']) + int(my_dataset[i]['from_this_person_to_poi']))
+    else:
+        my_dataset[i]['to_messages_poi_share'] = 'NaN'
+    if my_dataset[i]['from_poi_to_this_person'] != 'NaN' and my_dataset[i]['to_messages'] != 'NaN':
+        my_dataset[i]['from_messages_poi_share'] = int(my_dataset[i]['from_poi_to_this_person']) / (int(my_dataset[i]['to_messages']) + int(my_dataset[i]['from_poi_to_this_person']))
+    else:
+        my_dataset[i]['from_messages_poi_share'] = 'NaN'
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
@@ -33,7 +49,13 @@ labels, features = targetFeatureSplit(data)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+#clf = GaussianNB()
+
+from sklearn import tree
+#clf = tree.DecisionTreeClassifier()
+
+from sklearn.svm import SVC
+#clf = SVC()
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -43,9 +65,15 @@ clf = GaussianNB()
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+
+#clf = GaussianNB()
+
+clf = tree.DecisionTreeClassifier(max_features=2, min_samples_split=2, criterion='entropy')
+
+#clf = SVC(kernel='rbf', C=10000)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
